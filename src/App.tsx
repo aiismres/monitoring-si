fc76ec4c428f55506202f38050436f9e4ad1e1bb
Aleksey Grachev <GrachevAI@gazpromenergosbyt.ru>
2023-04-14 16:26:57 +0300

    add clipboard test

--- src/App.tsx
+++ src/App.tsx
@@ -497,6 +497,36 @@ function App() {
             setAppState={setAppState}
             setSechInfo={setSechInfo}
           />
+          <input
+            type="text"
+            onPaste={(e) => {
+              console.log(e.clipboardData.getData('Text'));
+              console.log(e.clipboardData.getData('text/plain'));
+              console.log(e.clipboardData.getData('text/html'));
+              console.log(e.clipboardData.getData('text/rtf'));
+            }}
+          />
+          <button
+            onClick={(e) => {
+              navigator.clipboard
+                .readText()
+                .then((text) => {
+                  console.log('Pasted content: ');
+                  const rows = text
+                    .split('\r\n')
+                    .forEach((row) => console.log(row.split('\t')));
+                  console.log(rows);
+                })
+                .catch((err) => {
+                  console.error('Failed to read clipboard contents: ', err);
+                });
+              // console.log(e.clipboardData.getData('text/plain'));
+              // console.log(e.clipboardData.getData('text/html'));
+              // console.log(e.clipboardData.getData('text/rtf'));
+            }}
+          >
+            paste
+          </button>
           <SpeedDialNav
             btnExportSv1={btnExportSv1}
             // inputFileSv2={inputFileSv2}
