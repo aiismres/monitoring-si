479a47e4360f8b4d0bafa1c22be5e83c028dffab
Aleksey Grachev <GrachevAI@gazpromenergosbyt.ru>
2023-10-17 15:52:39 +0300

    minor improvement

--- src/MonitoringSi/MonitoringSi.tsx
+++ src/MonitoringSi/MonitoringSi.tsx
@@ -396,7 +396,7 @@ export function MonitoringSi({
   }
 
   function handleKeyDown(e: React.KeyboardEvent<HTMLTableSectionElement>) {
-    if (appState.isEdit) return;
+    if (appState.isEdit || !appState.isEdit2) return;
     console.log(e, 'e.code', e.code, 'e.key', e.key);
     const { i, param } = selectedItems[0] || { i: null, param: null };
 
